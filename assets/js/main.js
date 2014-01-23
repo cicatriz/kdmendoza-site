97e19ef4f0e8c117d852e4ce27d6dda31ba93b4a
cicatriz <ryan@geknowm.com>
2014-01-23 13:27:11 -0600

    better spacing for magellan links

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ $(document).foundation("magellan", {threshold: 150});
 $(document).ready(function() {
   $('.magellan a').on('click', function(){
       var elementID = $(this).attr('href').substring(1);
-      var pos = $("[data-magellan-destination="+elementID+"]").offset().top - 65;
+      var pos = $("[data-magellan-destination="+elementID+"]").offset().top - 100;
       console.log("scrolling to",pos);
       $.scrollTo({ endY: pos });
       return false;
